81303a04ec1ec7da8e943e52a7317ee489b4db9d
Aysha-Codingal <arshad6369@gmail.com>
2025-11-15 13:37:52 +0500

    My first pygame program

--- Pygame/Class 1/Adding Image.py
+++ Pygame/Class 1/Adding Image.py
@@ -4,12 +4,14 @@ pygame.init()
 
 window = pygame.display.set_mode((300,400))
 window.fill("cyan")
+bg = pygame.transform.scale(pygame.image.load("RedSelector.png").convert(),(300,400))
 
 done = False
 while not done:
     for event in pygame.event.get():
         if event.type ==pygame.QUIT:
             pygame.quit()
+        window.blit(bg,(0,0))            
         
 
     pygame.display.flip()      
