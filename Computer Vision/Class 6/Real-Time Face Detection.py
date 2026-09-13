14dec1003a43ee11e52e097bb04f29b1a8422fec
Aysha-Codingal <arshad6369@gmail.com>
2026-09-13 15:29:20 +0500

    Computer Vision Class 6

---
+++ Computer Vision/Class 6/Real-Time Face Detection.py
@@ -0,0 +1,30 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Error Could No Open WebCam!")
+    exit()
+
+while True:
+
+    ret,frame = cap.read()
+
+    if not ret:
+        print("Error: Failed to Capture image")
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray,scaleFactor = 1.1, minNeighbors = 8, minSize = (30,30))
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x, y),(x+y+w+h), (0,0,255),2)
+
+        font = cv2.FONT_HERSHEY_COMPLEX
+        cv2.putText(frame, f'people Count: {len(faces)}',(10, 30),font,1,(0,0,255),2,cv2.LINE_AA) 
+        cv2.imshow("face tracking and count",frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()   
